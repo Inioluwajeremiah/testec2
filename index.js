c4a10093dc982ffe62a33bf05457c0331eea6335
Inioluwa <adewara@gmail.com>
2024-01-31 16:55:34 +0100

    port 5001

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 5000;
+const port = 5001;
 
 // app.use(cors);
 
-app.use("/api", (req, res) => {
+app.use("/api/", (req, res) => {
   res.send("Test app running");
 });
 
